a787bb7162a2ff468e7da979919a6900484a191f
Mike Luz <onlymikeluz@gmail.com>
2017-05-03 15:42:53 -0400

    closes #6

---
+++ client/Main.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {render} from 'react-dom';
+import {Provider} from 'react-redux';
+
+// import store from './store';
+// import Routes from './routes';
+
+const Main = () => {
+	<Provider store={store}>
+		<Routes />
+	</Provider>
+}
+
+render(
+  <Main />,
+  document.getElementById('app') // make sure this is the same as the id of the div in your index.html
+);
