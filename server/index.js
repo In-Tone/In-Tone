ede0e7c4a1a722e900e0192e360d1b3fc7c2a78b
Mike Luz <onlymikeluz@gmail.com>
2017-05-03 15:19:07 -0400

    closes #3 again

---
+++ server/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.use(require('volleyball'));
+
+express.static(path.join(__dirname, '../public'));
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.listen(3000, function () {
+  console.log("Knock, knock");
+  console.log("Who's there?");
+  console.log("Your server, listening on port 3000");
+});
+
+app.use('/api', require('./api/api'));
+
+app.get('*', function (req, res) {
+  res.sendFile(path.join(__dirname, '../../public'));
+});
+
+app.use(function (err, req, res, next) {
+  console.error(err);
+  console.error(err.stack);
+  res.status(err.status || 500).send(err.message || 'Internal server error.');
+});
