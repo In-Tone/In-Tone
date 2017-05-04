d4dc54e3008f2abcb3faaee6c79a53c809eb2807
ekotwick <edmond.kotwick@gmail.com>
2017-05-03 21:16:15 -0400

    closes #9

--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,14 @@ module.exports = {
         options: {
           presets: ['react', 'es2015'] // if you aren't using 'babel-preset-es2015', then omit the 'es2015'
         }
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
       }
     ]
   }
