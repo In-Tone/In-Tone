3a3aab1a227a3c05ced9382f0e18975574f4090b
ekotwick <edmond.kotwick@gmail.com>
2017-05-05 10:34:26 -0400

    closes #31

--- server/index.js
+++ server/index.js
@@ -3,24 +3,26 @@ const app = express();
 const path = require('path');
 const session = require('express-session');
 const passport = require('passport');
-const volleyball = require('volleyball');
+// const volleyball = require('volleyball');
 
-app.use(volleyball);
+// app.use(volleyball);
 
-express.static(path.join(__dirname, '../public'));
+const port = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, '..', 'public')));
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.listen(3000, function () {
+app.listen(port, function () {
   console.log("Knock, knock");
   console.log("Who's there?");
   console.log("Your server, listening on port 3000");
 });
 
 app.use(session({
-	secret: process.env.SESSION_SECRET,
+	secret: process.env.SESSION_SECRET || 'remove me',
 	resave: false, // does not force session to be saved to store
 	saveUnitialized: false // does not force new sessions to be saved to the store
 }));
@@ -32,7 +34,7 @@ app.use(passport.session());
 app.use('/api', require('./api/api'));
 
 app.get('*', function (req, res) {
-  res.sendFile(path.join(__dirname, '../public'));
+  res.sendFile(path.join(__dirname, '..', 'public'));
 });
 
 app.use(function (err, req, res, next) {
