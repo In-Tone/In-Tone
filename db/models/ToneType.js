dd33b0f2639fef86f67f1f36db7c3efca53da117
ekotwick <edmond.kotwick@gmail.com>
2017-05-08 15:53:12 -0400

    closes #60

---
+++ db/models/ToneType.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const {ENUM} = require('sequelize');
+
+module.exports = db => db.define('userTone', {
+	language: {
+		type: ENUM('thai','chinese','vietnamese','hmong'),
+		allowNull: false
+	},
+	tone: {
+		type: ENUM('high', 'mid', 'low', 'falling', 'rising', 'dipping', 'peaking'),
+		allowNull: false
+	}
+});
+
+module.exports.associations = (ToneType, {UserTone, Target}) => {
+	ToneType.belongsToMany(UserTone)
+	ToneType.belongsToMany(Target)
+};
