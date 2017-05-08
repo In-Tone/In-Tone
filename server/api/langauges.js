a5b1c082caa151edb4be0f49e35a7ffee4c997bc
ekotwick <edmond.kotwick@gmail.com>
2017-05-08 18:26:37 -0400

    Update langauges.js

--- server/api/langauges.js
+++ server/api/langauges.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const db = require('../../db');
-const ToneType = db.model('userTone');
+const ToneType = db.model('toneType');
 const Target = db.model('target');
 
 module.exports = require('express').Router()
