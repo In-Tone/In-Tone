b9170d73dec63779a92ae52c89e0caebe3d6434a
ekotwick <edmond.kotwick@gmail.com>
2017-05-08 12:24:03 -0400

    Update tones.js

--- server/api/tones.js
+++ server/api/tones.js
@@ -1,13 +1,13 @@
 'use strict'
 
 const router = require('express').Router();
-const UserTone = require('../../db/models/userTone');
+const userTones = require('../../db/models/userTones');
 const {mustBeLoggedIn} = require('../auth');
 
 // store a pitch contour after registering it
 // payload received must be an object with {category: 'string', data: [pitchContourData]}
 router.post('/contour', mustBeLoggedIn, (req, res, next) => {
-	UserTone.create(req.body)
-		.then(userTone => res.status(201).json(userTone))
+	UserTones.create(req.body)
+		.then(userTones => res.status(201).json(userTones))
 		.catch(next)
 });
