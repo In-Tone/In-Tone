31371b45aa89fdde4851a302d501e8ce1dc3ab60
ekotwick <edmond.kotwick@gmail.com>
2017-05-08 12:22:56 -0400

    Update and rename contours.js to tones.js

--- server/api/tones.js
+++ server/api/tones.js
@@ -1,13 +1,13 @@
 'use strict'
 
 const router = require('express').Router();
-const Contour = require('../../db/models/Contour');
+const UserTone = require('../../db/models/userTone');
 const {mustBeLoggedIn} = require('../auth');
 
 // store a pitch contour after registering it
 // payload received must be an object with {category: 'string', data: [pitchContourData]}
 router.post('/contour', mustBeLoggedIn, (req, res, next) => {
-	Contour.create(req.body)
-		.then(contour => res.status(201).json(contour))
+	UserTone.create(req.body)
+		.then(userTone => res.status(201).json(userTone))
 		.catch(next)
 });
