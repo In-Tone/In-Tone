61f701eff47a196e49db41bd356a3ac92a80e124
ekotwick <edmond.kotwick@gmail.com>
2017-05-08 21:56:55 -0400

    Update index.js

--- db/index.js
+++ db/index.js
@@ -1,12 +1,3 @@
-// const Sequelize = require('sequelize');
-
-// const db = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost:5432/intone', {
-//   logging: false // unless you like the logs
-//   // ...and there are many other options you may want to play with
-// });
-
-// module.exports = db;
-
 const Sequelize = require('sequelize');
 const url = process.env.DATABASE_URL || 'postgres://localhost:5432/intone';
 
