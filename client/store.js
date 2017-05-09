1046b2b5a18ae1e369cf350c0f74cc0c4a1c4418
ekotwick <edmond.kotwick@gmail.com>
2017-05-09 13:43:32 -0400

    closes #93

--- client/store.js
+++ client/store.js
@@ -2,6 +2,7 @@ import { createStore, applyMiddleware } from 'redux';
 import rootReducer from './reducers/index';
 import thunkMiddleware from 'redux-thunk';
 import { createLogger } from 'redux-logger';
+import { composeWithDevTools } from 'redux-devtools-extension'
 
 const store = createStore(
   rootReducer,
