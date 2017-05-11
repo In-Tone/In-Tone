26e79b289a09fecdc2f78115bd1ea6729c4dcec9
ekotwick <edmond.kotwick@gmail.com>
2017-05-11 12:06:25 -0400

    closes #140

---
+++ untitled.js
@@ -0,0 +1,12 @@
+      targets: [],
+      // information for current target
+      duration: '',
+      englishTranslation: '',
+      language: '',
+      nativeSpelling: '',
+      pitches: [],
+      tone: '',
+      tone_type_id: 0, // id for toneType db model
+      toneId: '', // id for Target db model
+      transliteration: '',
+      wav: ''
