93df7f97d9b5642f5cb7eec54e05f440feedfab8
ekotwick <edmond.kotwick@gmail.com>
2017-05-11 15:02:30 -0400

    changed seed file

--- db/seed.js
+++ db/seed.js
@@ -96,7 +96,7 @@ const toneTypes = seed(ToneType, {
 
 const targets = seed(Target, ({toneTypes}) =>  ({
 	fallingChai: {
-		wav: 'http://localhost:3000/audio/Falling-Chai-Yes.wav',
+		wav: 'https://s3.amazonaws.com/in-tone/Falling-Chai-Yes.wav',
 		pitches: [0,0,0,0,91,94,94,94,94,283,286,283,281,281,281,279,279,279,281,281,283,281,281,283,283,285,283,279,276,269,259,245,233,214,197,180,168,159,151,147,140,143],
 		duration: 641.587,
 		englishTranslation: 'yes',
@@ -105,7 +105,7 @@ const targets = seed(Target, ({toneTypes}) =>  ({
 		tone_type_id: 3
 	},
 	fallingHai: {
-		wav: 'http://localhost:3000/audio/Falling-Hai-Give.wav',
+		wav: 'https://s3.amazonaws.com/in-tone/Falling-Hai-Give.wav',
 		pitches: [83,87,87,87,87,87,87,87,87,92,281,281,283,281,277,276,276,276,276,277,277,279,279,281,281,277,277,272,271,264,261,255,246,231,208,193,170,155,145,142,142,142],
 		duration: 645.029,
 		englishTranslation: 'give',
@@ -114,7 +114,7 @@ const targets = seed(Target, ({toneTypes}) =>  ({
 		tone_type_id: 3
 	},
 	highChai: {
-		wav: 'http://localhost:3000/audio/High-Chai-Use.wav',
+		wav: 'https://s3.amazonaws.com/in-tone/High-Chai-Use.wav',
 		pitches: [83,83,88,83,83,83,85,85,89,258,256,255,253,249,245,242,238,236,236,235,233,235,235,235,235,235,236,238,241,249,255,261,266,276,285,292,296,302,304,302,290,136,127,119,90],
 		duration: 688.256,
 		englishTranslation: 'use',
@@ -123,7 +123,7 @@ const targets = seed(Target, ({toneTypes}) =>  ({
 		tone_type_id: 4
 	},
 	highYai: {
-		wav: 'http://localhost:3000/audio/High-Yai-Move.wav',
+		wav: 'https://s3.amazonaws.com/in-tone/High-Yai-Move.wav',
 		pitches: [0,219,218,225,244,246,256,122,248,244,242,241,238,237,237,237,238,238,237,236,235,235,236,235,235,233,233,235,236,236,238,240,244,246,251,256,261,269,279,286,296,302,308,311,308,300,292,281,266,255,230,90],
 		duration: 788.361,
 		englishTranslation: 'move',
@@ -132,7 +132,7 @@ const targets = seed(Target, ({toneTypes}) =>  ({
 		tone_type_id: 4
 	},
 	lowLhai: {
-		wav: 'http://localhost:3000/audio/Low-Lhai-Shoulder.wav',
+		wav: 'https://s3.amazonaws.com/in-tone/Low-Lhai-Shoulder.wav',
 		pitches: [0,226,219,219,218,213,212,204,200,196,193,188,186,184,181,178,174,172,170,168,169,168,168,168,167,165,164,162,159,155,148,145,142,136,139,146,141,82,84,84,168,169,178,186,90,89],
 		duration: 695.739,
 		englishTranslation: 'shoulder',
@@ -141,7 +141,7 @@ const targets = seed(Target, ({toneTypes}) =>  ({
 		tone_type_id: 2
 	},
 	lowMai: {
-		wav: 'http://localhost:3000/audio/Low-Mai-New.wav',
+		wav: 'https://s3.amazonaws.com/in-tone/Low-Mai-New.wav',
 		pitches: [0,211,205,206,208,208,106,207,205,204,200,197,195,192,188,185,181,178,174,171,168,166,166,165,165,164,164,164,160,158,155,153,152,153,154,153,151,153,151,155,156,158,163,168],
 		duration: 674.849,
 		englishTranslation: 'new',
@@ -159,7 +159,7 @@ const targets = seed(Target, ({toneTypes}) =>  ({
 		tone_type_id: 1
 	},
 	midPai: {
-		wav: 'http://localhost:3000/audio/Mid-Pai-Go.wav',
+		wav: 'https://s3.amazonaws.com/in-tone/Mid-Pai-Go.wav',
 		pitches: [83,232,231,227,227,226,225,225,225,224,224,224,223,223,221,218,218,215,213,211,210,211,210,210,211,211,211,211,211,212,211,210,211,212,213,215,216,218,218,85],
 		duration: 600.686,
 		englishTranslation: 'go',
@@ -168,7 +168,7 @@ const targets = seed(Target, ({toneTypes}) =>  ({
 		tone_type_id: 1
 	},
 	risingNhai: {
-		wav: 'http://localhost:3000/audio/Rising-Nhai-Where.wav',
+		wav: 'https://s3.amazonaws.com/in-tone/Rising-Nhai-Where.wav',
 		pitches: [0,227,218,217,217,217,218,116,215,211,208,207,202,199,195,192,188,182,88,87,170,171,170,170,170,171,172,173,173,175,178,181,187,193,203,216,231,123,264,286,302,322,339,353,359,359,361,361,361],
 		duration: 736.719,
 		englishTranslation: 'where',
@@ -177,7 +177,7 @@ const targets = seed(Target, ({toneTypes}) =>  ({
 		tone_type_id: 5
 	},
 	risingSai: {
-		wav: 'http://localhost:3000/audio/Rising-Sai-Clear.wav',
+		wav: 'https://s3.amazonaws.com/in-tone/Rising-Sai-Clear.wav',
 		pitches: [0,0,0,0,0,0,0,0,0,84,248,237,231,228,227,223,215,105,204,101,197,193,187,185,181,181,182,183,183,185,186,188,188,193,200,212,230,251,269,290,306,322,337,345,353,353,119,119,119,119,119,119,119],
 		duration: 796.222,
 		englishTranslation: 'clear',
