e31ed818bc453f1d0eec13ecffc4984c19035672
Pimpila <pimpila@gmail.com>
2017-05-13 15:54:30 -0400

    modals closing

--- client/components/Footer.js
+++ client/components/Footer.js
@@ -14,16 +14,10 @@ import Paper from 'material-ui/Paper';
 import SkyLight from 'react-skylight';
 
 import store from '../store';
-import {setModalVisibility} from '../reducers/Modals';
 
 class Footer extends Component {
 	constructor() {
 		super()
-
-		// this.state = {
-		// 	modalVisible: true
-		// }
-
 	}
 
 	onStudyClick() {
@@ -31,21 +25,6 @@ class Footer extends Component {
 	}
 
 	render() {
-		console.log('this.state: ', this.state)
-
-		var myDialog = {
-      backgroundColor: '#00897B',
-      color: '#ffffff',
-      width: '70%',
-      height: '450px',
-      marginTop: '-300px',
-      marginLeft: '-35%',
-			fontSize: '28px',
-			padding: '50px'
-    };
-
-				console.log('Skylight props: ', SkyLight.propTypes)
-
 		return (
 			<footer className="col-xs-12" style={styles.footer}>
 
@@ -54,26 +33,42 @@ class Footer extends Component {
           	Tonal languages use pitch to distinguish a word's meaning. Depending on the pitch, the same syllable could mean two totally different things! For example, in Thai, <i>ma</i> spoken in a high tone would mean <i>horse</i> and in a rising tone would mean <i>dog</i>. For people who don't speak tonal languages, these tones are hard to perceive and reproduce.
         	</SkyLight>
 					<FlatButton onClick={() => this.refs.howItWorks.show()} className="col-xs-4" style={styles.button}>How In-Tone Works</FlatButton>
-					<SkyLight isVisible={this.props.modalVisible} dialogStyles={myDialog} hideOnOverlayClicked ref="howItWorks" title="How In-Tone Works">
+					<SkyLight  dialogStyles={myDialog} hideOnOverlayClicked ref="howItWorks" title="How In-Tone Works">
           	In-Tone helps you perceive and reproduce a language's tones. You are given a series of vocabulary flashcards with audio of the word being spoken by a native speaker. You can then record yourself saying the word, and In-Tone will provide you with a graph comparing the target's pitch values alongside your own.
 						<br />
-						<FlatButton onClick={() => this.refs.train.show()} style={styles.overlayButton}><u>Train Now</u></FlatButton>
+						<FlatButton
+							onClick={() => {
+								this.refs.train.show()
+							}}
+							style={styles.overlayButton}><u>Train Now</u></FlatButton>
 						<SkyLight dialogStyles={myDialog} hideOnOverlayClicked ref="train" title="Choose a Language:">
 							<div style={styles.buttonSection}>
 								<FlatButton style={styles.overlayButton}><u>Mandarin</u></FlatButton>
 								<br />
-								<FlatButton onClick={() => this.refs.mode.show()} style={styles.overlayButton}><u>Thai</u></FlatButton>
+								<FlatButton
+									onClick={() => {
+										this.refs.mode.show()
+
+									}}
+									style={styles.overlayButton}>
+									<u>Thai</u>
+								</FlatButton>
 								<br />
 								<FlatButton style={styles.overlayButton}><u>Hmong</u></FlatButton>
 								<br />
 							</div>
 						</SkyLight>
         	</SkyLight>
-					<SkyLight isVisible={this.props.modalVisible} dialogStyles={myDialog} hideOnOverlayClicked ref="mode" title="Choose a Training Mode:">
+					<SkyLight dialogStyles={myDialog} hideOnOverlayClicked ref="mode" title="Choose a Training Mode:">
 						<FlatButton style={styles.overlayButton}><u>Play</u></FlatButton>
 						<br />
 						{/* need to change Study button to link to Study but study right now is broken. Need to set the modal to be invisible once this is clicked*/}
-						<Link to='/modes'><FlatButton onClick={this.onStudyClick} style={styles.overlayButton}><u>Study</u></FlatButton></Link>
+						<Link to='/modes'><FlatButton onClick={() => {
+							this.refs.train.hide()
+							this.refs.howItWorks.hide()
+							this.refs.mode.hide()
+							}}
+						style={styles.overlayButton}><u>Study</u></FlatButton></Link>
 						<br />
 					</SkyLight>
 					<Link to='/modes'><FlatButton className="col-xs-4" style={styles.button}>Train</FlatButton></Link>
@@ -109,6 +104,18 @@ const styles = {
 		marginTop: '30px'
 	}
 }
+
+const myDialog = {
+	// backgroundColor: '#00897B',
+	backgroundColor: 'rgba(0, 137, 123, 1)',
+	color: '#ffffff',
+	width: '70%',
+	height: '450px',
+	marginTop: '-300px',
+	marginLeft: '-35%',
+	fontSize: '28px',
+	padding: '50px'
+};
 
 
 const mapStateToProps = state => ({
--- client/reducers/index.js
+++ client/reducers/index.js
@@ -1,12 +1,11 @@
 import { combineReducers } from 'redux';
 import languageReducer from './Language';
 import targetsReducer from './Targets';
-import modalsReducer from './Modals';
+
 
 const rootReducer = combineReducers({
   language: languageReducer,
   allTargets: targetsReducer,
-  modalVisible: modalsReducer
 });
 
 export default rootReducer;
