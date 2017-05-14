b02f76d1b422058cb8cf69b26a8c7c7a70fc3194
Pimpila <pimpila@gmail.com>
2017-05-14 11:45:47 -0400

    fix font size

--- client/components/Footer.js
+++ client/components/Footer.js
@@ -32,20 +32,22 @@ class Footer extends Component {
 			<footer className="col-xs-12" style={styles.footer}>
 					<FlatButton onClick={() => this.refs.tonalLanguages.show()} className="col-xs-4" style={styles.button}>What are Tonal Languages?</FlatButton>
 						<SkyLight dialogStyles={styles.myDialog} hideOnOverlayClicked ref="tonalLanguages" title="What are Tonal Languages?">
-							Tonal languages use pitch to distinguish a word's meaning. Depending on the pitch, the same syllable could mean two totally different things! For example, in Thai, <i>ma</i> spoken in a high tone would mean <i>horse</i> and in a rising tone would mean <i>dog</i>. For people who don't speak tonal languages, these tones are hard to perceive and reproduce.
+							<p>Tonal languages use pitch to distinguish a word's meaning. Depending on the pitch, the same syllable could mean two totally different things! For example, in Thai, <i>ma</i> spoken in a high tone would mean <i>horse</i> and in a rising tone would mean <i>dog</i>. For people who don't speak tonal languages, these tones are hard to perceive and reproduce.</p>
 						</SkyLight>
 					<FlatButton onClick={() => this.refs.howItWorks.show()} className="col-xs-4" style={styles.button}>How In-Tone Works</FlatButton>
 					<SkyLight  dialogStyles={styles.myDialog} hideOnOverlayClicked ref="howItWorks" title="How In-Tone Works">
-          	In-Tone helps you perceive and reproduce a language's tones. You are given a series of vocabulary flashcards with audio of the word being spoken by a native speaker. You can then record yourself saying the word, and In-Tone will provide you with a graph comparing the target's pitch values alongside your own.
-						<br />
-						<RaisedButton
-							label='Train Now'
-							labelStyle={{fontSize: '24px'}}
-							onClick={() => {
-								console.log('this inside train now button: ', this)
-								this.refs.train.show()
-							}}
+						<div>
+							<p>In-Tone helps you perceive and reproduce a language's tones. You are given a series of vocabulary flashcards with audio of the word being spoken by a native speaker. You can then record yourself saying the word, and In-Tone will provide you with a graph comparing the target's pitch values alongside your own.</p>
+							<br />
+							<RaisedButton
+								label='Train Now'
+								labelStyle={{fontSize: '24px'}}
+								onClick={() => {
+									console.log('this inside train now button: ', this)
+									this.refs.train.show()
+								}}
 							style={styles.modalButton} />
+							</div>
 						<SkyLight dialogStyles={styles.myDialog} hideOnOverlayClicked ref="train" title="Choose a Language:">
 							<div style={styles.buttonSection}>
 								<RaisedButton label='Mandarin' labelStyle={{fontSize: '24px'}} style={styles.modalButton} disabled={true}/>
@@ -99,18 +101,20 @@ const styles = {
 	},
 	button: {
 		fontSize: '24px',
-		color: 'white',	
+		color: 'white',
 		display: 'in-line',
 		height: '100%',
 	},
 	modalButton: {
 		color: 'white',
 		margin: '30px',
-
 	},
 	buttonSection: {
 		marginTop: '40px',
 		// paddingTop: '30px'
+	},
+	textSection: {
+		position: 'absolute',
 	},
 	myDialog: {
 	// backgroundColor: '#00897B',
@@ -118,14 +122,14 @@ const styles = {
 		color: '#ffffff',
 		width: '70%',
 		height: '450px',
-		fontSize: '28px',
+		fontSize: '20px',
 		paddingTop: '50px',
 		paddingLeft: '75px',
 		paddingRight: '75px',
 		position: 'fixed',
 		zIndex: 999999999999999,
 	  // line spacing that's easier to read
-    lineHeight: '45px',
+    lineHeight: '32px',
     // vertical alignment
     top:0,
     bottom: 0,
