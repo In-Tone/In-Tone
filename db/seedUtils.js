d342abbe2fb8a47e011f58cf3720ac1a0f8f2933
ekotwick <edmond.kotwick@gmail.com>
2017-05-16 13:43:13 -0400

    added chance.js and began seedutils file

---
+++ db/seedUtils.js
@@ -0,0 +1,26 @@
+// id: 4
+
+export const targetToToneType = {
+	1: 2,
+	2: 4,
+	3: 3,
+	4: 4,
+	5: 3,
+	6: 2,
+	7: 1,
+	8: 1,
+	9: 5,
+	10: 5
+}
+
+export const generatePerformanceData = () => {
+	let returnObj = {
+		pitches: [],
+		isBest: bool,
+		score: 0,
+		difficulty: '',
+		user_id: 4,
+		tone_type_id: 0,
+		target_id: 0,
+	}
+}
