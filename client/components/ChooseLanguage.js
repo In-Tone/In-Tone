7fc741b6a42e4dd3fd6a29f7da015f92049bdaad
Pimpila <pimpila@gmail.com>
2017-05-17 14:27:46 -0400

    first commit

--- client/components/ChooseLanguage.js
+++ client/components/ChooseLanguage.js
@@ -1,25 +1,26 @@
 import React from 'react';
+import { Link } from 'react-router';
 import Paper from 'material-ui/Paper';
 import RaisedButton from 'material-ui/RaisedButton';
 
 const ChooseLanguage = (props) => {
-	return (
-		<div style={styles.container}>
-				<div style={styles.content}>
-					<h1>Choose a language</h1>
-					<h2>(more coming soon!)</h2>
-					<RaisedButton
-						style={styles.modalButton}
-						label='Thai'
-						labelStyle={{ fontSize: '24px' }}
-						onClick={() => {
-							props.onLanguageClick("thai")
-						}} />
-
-				</div>
-		</div>
-	)
+  return (
+    <div style={styles.container}>
+        <div style={styles.content}>
+          <h1>Choose a language</h1>
+          <h3><i>( more coming soon! )</i></h3>
+          <Link to='/study'>
+          <img
+            style={styles.button}
+            src={'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Flag_of_Thailand.svg/1024px-Flag_of_Thailand.svg.png'}
+            onClick={() => props.onLanguageClick('thai')} />
+          </Link>
+          <h4>THAI</h4>
+        </div>
+    </div>
+  )
 }
+
 
 export default ChooseLanguage
 
