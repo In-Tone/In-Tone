35575490296a1b31fde18318bf8336cad6e8f9c7
ekotwick <edmond.kotwick@gmail.com>
2017-05-24 15:10:18 -0400

    integrate compatibility pop-up

---
+++ client/components/CompatibilityText.js
@@ -0,0 +1,17 @@
+'use strict';
+
+// react
+import React from 'react';
+
+const browserCompatible = () => (
+	<p>Your browser supports Web Audio! If, however, you do not have access to certain features of our app, please browse with Firefox or Chrome version 49 or higher.</p>
+);
+
+const browserIncompatible = () => (
+	<p>Unfortunately, your browser does not support all the features of our app. Please use Firefox or Chrome version 49 or higher</p>
+);
+
+export const compatibilityFlag = () => {
+	if (Modernizr.bloburls && Modernizr.webaudio) return browserCompatible();
+	else browserIncompatible();
+};
